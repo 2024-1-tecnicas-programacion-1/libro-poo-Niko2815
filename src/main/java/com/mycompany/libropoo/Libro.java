5c01669e95694d28115847b9e903e7702b8f57da
Niko2815 <146888012+Niko2815@users.noreply.github.com>
2024-04-22 10:32:35 -0500

    Update Libro.java

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,38 @@
 package com.mycompany.libropoo;
 
-public class Libro {
+import java.time.LocalDate;
+
+public class Libro {//abre class
+
+private String titulo;
+private String autor;
+private String genero;
+private int anoPublicacion;
+boolean leido;
     
-}
+public Libro(String titulo, String autor, String genero,int anoPublicacion){//abre constructor Complejidad Temporal: Tiempo Constante 0(1)
+this.titulo = titulo;
+this.autor = autor;
+this.genero = genero;
+this.anoPublicacion = anoPublicacion;
+this.leido = false;
+
+    }//cierra constructor
+
+public String mostrarInformacion(){//abre mostrar informacion Complejidad Temporal: Tiempo Constante 0(1)
+
+return "Titulo: " + titulo + " Autor: " + autor + " Genero: " + genero + 
+        " Año Publicacion: " + anoPublicacion; 
+       
+}//cierra mostrar informacion
+
+public void marcarLeido(){//abre marcar leido Complejidad Temporal: Tiempo Constante 0(1)
+leido = true;
+
+}//cierra marcar leido
+public boolean esAntiguo(){//abre es Antiguo Complejidad Temporal: Tiempo Constante 0(1)
+
+return (LocalDate.now().getYear()- anoPublicacion > 50);
+}//cierra es antiguo
+
+}//cierra class
